835952eb6f920f55a8dcac265ee89a1e185dcee4
ETRI-EdgeAnalytics <45547720+ETRI-EdgeAnalytics@users.noreply.github.com>
2019-12-17 10:28:10 +0900

    Add files via upload

---
+++ litecnn-ios-src-1.0/LightAIModelVerificationTests/LightAIModelVerificationTests.swift
@@ -0,0 +1,36 @@
+//
+//  LightAIModelVerificationTests.swift
+//  LightAIModelVerificationTests
+//
+//  Created by leeint on 2018. 10. 3..
+//  Copyright © 2018년 leeint. All rights reserved.
+//
+
+import XCTest
+@testable import LightAIModelVerification
+
+class LightAIModelVerificationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
